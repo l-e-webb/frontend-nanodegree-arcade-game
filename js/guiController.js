46feee1530b99f8f40b68b18fb6e32a1e4f70592
louis <louis.webb5@gmail.com>
2015-01-24 14:51:16 -0800

    Update comments accross the board.

--- js/guiController.js
+++ js/guiController.js
@@ -1,7 +1,15 @@
+/* gui object used to handle the opening screen and game over
+ * screen, along with their associated buttons.
+ */
+
 var global = global||this;
 var gui = {};
+//Difficulty initiated at normal.
 game.difficulty = "normal";
 
+/* Changes the character, called when the player clicks on a sprite.
+ * in opening menu.
+ */
 gui.selectChar = function () {
     var charid = this.id;
     $(".charImg").removeClass("selected");
@@ -9,6 +17,7 @@ gui.selectChar = function () {
     game.charSprite = "images/" + charid + ".png";
 }
 
+//Ditto for difficulty in opening menu.
 gui.selectDifficulty = function () {
     var diffid = this.id;
     if (diffid != "play") {
@@ -18,16 +27,33 @@ gui.selectDifficulty = function () {
     }
 }
 
-var startGame = function() {    
-    game.player = new Player(game.charSprite);
-    game.allEnemies = game.initEnemies();
-    game.allGems = game.initGems();
-    Engine(global);
-    $(".main").remove();
-    game.running = true;
+/* Called when the player clicks the "Play Again" button from
+ * game over screen.  Currently uses the lazy option of simply reloading
+ * the page.
+ */
+gui.reset = function() {
+	location.reload();
 }
 
+/* Called when the player clicks "Play!"  Initiates the game, hides the
+ * opening menu, and calls the Engine function.
+ */
+gui.startGame = function() {
+	game.init();
+    $("#openingScreen").css("display", "none");
+    Engine(global);
+}
 
+/* Called when the player runs out of lives.  Removes canvas and shows
+ * the game over screen.
+ */
+game.over = function() {
+	$("canvas").remove();
+	$("#gameOverScreen").css("display", "flex");
+}
+
+//jQuery functions to handle on-click effects for menus.
 $(".charImg").click(gui.selectChar);
 $(".button").click(gui.selectDifficulty);
-$(".play-button").click(startGame);
+$("#playAgain").click(gui.reset);
+$("#play").click(gui.startGame);
